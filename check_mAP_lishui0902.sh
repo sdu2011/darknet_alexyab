681fadb0cb3ccb977d537f934395b41032541f71
Your Name <Your Email>
2020-09-27 14:19:00 +0800

    增加shell脚本

--- check_mAP_lishui0902.sh
+++ check_mAP_lishui0902.sh
@@ -1,3 +1,7 @@
 ./darknet detector map data/lishui_0902/trafficlights2.data cfg/yolov3-giou-lishui0902.cfg data/lishui_0902/trained_weights/yolov3-giou-lishui0902_last.weights -iou_thresh 0.75
 
 ./darknet detector map data/lishui_0908/trafficlights2.data cfg/lishui_0908/yolov3-giou.cfg data/lishui_0908/trained_weights/yolov3-giou_last.weights -iou_thresh 0.75
+
+./darknet detector map data/lishui_0914/trafficlights2.data cfg/lishui_0914/yolov3-giou.cfg data/lishui_0914/trained_weights/yolov3-giou_last.weights -iou_thresh 0.75
+
+./darknet detector map data/lishui_0927/trafficlights2.data cfg/lishui_0927/yolov3-giou.cfg data/lishui_0927/trained_weights/yolov3-giou_last.weights -iou_thresh 0.75
---
+++ train_lishui.sh
@@ -0,0 +1,15 @@
+# ./darknet detector train data/lishui_0902/trafficlights2.data cfg/yolov3-giou-lishui0902.cfg darknet53.conv.74 -mAP -gpus 2,3 -dont_show
+
+#在多个gpu上训练
+# Train it first on 1 GPU for like 1000 iterations: darknet.exe detector train cfg/coco.data cfg/yolov4.cfg yolov4.conv.137
+# Then stop and by using partially-trained model /backup/yolov4_1000.weights run training with multigpu (up to 4 GPUs): darknet.exe detector train cfg/coco.data cfg/yolov4.cfg /backup/yolov4_1000.weights -gpus 0,1,2,3
+# If you get a Nan, then for some datasets better to decrease learning rate, for 4 GPUs set learning_rate = 0,00065 (i.e. learning_rate = 0.00261 / GPUs). In this case also increase 4x times burn_in = in your cfg-file. I.e. use burn_in = 4000 instead of 1000.
+
+
+./darknet detector train data/lishui_0902/trafficlights2.data cfg/lishui_0902/yolov3-giou-lishui0902.cfg data/lishui_0902/trained_weights/yolov3-giou-lishui0902_last.weights -mAP -gpus 2,3 -dont_show
+
+./darknet detector train data/lishui_0908/trafficlights2.data cfg/lishui_0908/yolov3-giou.cfg darknet53.conv.74 -mAP -gpus 2,3 -dont_show
+
+./darknet detector train data/lishui_0914/trafficlights2.data cfg/lishui_0914/yolov3-giou.cfg darknet53.conv.74 -mAP -gpus 2,3 -dont_show
+
+./darknet detector train data/lishui_0927/trafficlights2.data cfg/lishui_0927/yolov3-giou.cfg darknet53.conv.74 -mAP -gpus 2,3 -dont_show
